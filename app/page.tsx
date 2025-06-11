ca954f5055f75e3552c3ef3e328cca43cd789c88
Tri Ngo <tringo831@gmail.com>
2025-06-11 13:48:56 -0700

    Add homeBanner

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Container from "@/components/Container";
+import HomeBanner from "@/components/ui/HomeBanner";
 
 export default function Home() {
   return (
     <Container className="">
-      <h2 className="text-2xl">Home</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias
-        voluptatem, blanditiis similique quaerat quisquam, vero ratione enim eos
-        autem est minus quod perferendis quos odit repellat deserunt tempore!
-        Praesentium, incidunt? Debitis voluptatibus culpa amet cum, sit
-        veritatis labore temporibus, adipisci consequatur, odio at. Tenetur
-        doloremque commodi eveniet consectetur exercitationem distinctio?
-      </p>
-      <Button>Click me</Button>
+      <HomeBanner />
     </Container>
   );
 }
